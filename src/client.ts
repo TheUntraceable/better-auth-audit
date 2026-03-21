f905e17cb5ab53963e4559fc1b8f49c941b6941b
Ridhwan Hussain <73362400+TheUntraceable@users.noreply.github.com.>
2026-03-21 17:27:00 +0000

    Make type of plugin nicer

--- src/client.ts
+++ src/client.ts
@@ -1,9 +1,9 @@
 import type { BetterAuthClientPlugin } from "better-auth/client";
-
+import type { auditLog } from "./index";
 export const auditLogClient = () => {
   return {
     id: "audit-log",
-    $InferServerPlugin: {} as ReturnType<typeof import("./index").auditLog>,
+    $InferServerPlugin: {} as ReturnType<typeof auditLog>,
     pathMethods: {
       "/audit-log/logs": "GET",
     },
